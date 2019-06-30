d6ba38c564bb5fd2e842fc06542657977d9fc32e
Guillaume Vagner <guillaume.vagner@supelec.fr>
2019-06-30 19:17:02 +0200

    fix substring

--- VerifyToken.js
+++ VerifyToken.js
@@ -2,13 +2,14 @@ var jwt = require('jsonwebtoken');
 var config = require('./config');
 
 function verifyToken(req, res, next) {
-  var token = req.headers['authorization'].substring(7);
-  console.log(token);
 
-  if (!token)
+
+  if (!req.headers['authorization'])
     return res.status(403).send({ auth: false, message: 'No token provided.' });
 
-  jwt.verify(token, config.cred.authsecret, function(err, decoded) {
+  var token = req.headers['authorization'].substring(7);
+
+  jwt.verify(token, config.cred.authsecret, function (err, decoded) {
     if (err) return res.status(401).send({ auth: false, message: 'Failed to authenticate token.' });
 
     // if everything good, save to request for use in other routes
